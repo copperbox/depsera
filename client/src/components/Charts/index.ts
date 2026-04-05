5c2e3632931f56b06e208e5307324a523d38ea78
Dan Essig <dan.essig@gmail.com>
2026-04-04 22:15:29 -0700

    DPS-96: add ApiKeyUsageChart component with time range selector

--- client/src/components/Charts/index.ts
+++ client/src/components/Charts/index.ts
@@ -1,3 +1,4 @@
+export { ApiKeyUsageChart } from './ApiKeyUsageChart';
 export { LatencyChart } from './LatencyChart';
 export { HealthTimeline } from './HealthTimeline';
 export { TimeRangeSelector } from './TimeRangeSelector';
